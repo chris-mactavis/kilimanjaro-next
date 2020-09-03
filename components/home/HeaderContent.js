c10388184c22d7b777343678c64053fa9f370257
khrisbreezy <khristopherabolarin@gmail.com>
2020-09-03 12:11:39 +0100

    Added the header and its content

---
+++ components/home/HeaderContent.js
@@ -0,0 +1,40 @@
+import Select from 'react-select';
+
+const HeaderContent = () => {
+    const states = [
+        {value: 'Lagos', label: 'Lagos'}
+    ];
+
+    const restaurants = [
+        {value: 'Kilimanjaro Lagos', label: 'Kilimanjaro Lagos'}
+    ];
+
+
+    return (
+      <>
+        <section className="header-content">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8 mx-auto">
+                        <div className="headtext-container">
+                            <h1>
+                                Enjoy tasty meals, <br/>
+                                from wherever you are.
+                            </h1>
+                            <p>Ordering from:</p>
+                            <form className="select-state">
+                                <Select options={states} className="select-tool" placeholder='Select State' />
+                                <Select options={restaurants} className="select-tool" placeholder='Select Restaurant' />
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <img className="left-banner-image" src="/images/left-banner-image.svg" alt=""/>
+            <img className="right-banner-image" src="/images/right-banner-image.svg" alt=""/>
+        </section>
+      </>  
+    );
+};
+
+export default HeaderContent;
