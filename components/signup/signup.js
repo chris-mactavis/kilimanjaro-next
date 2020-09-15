58f516796610e11815ab625792657e60e677c8ea
khrisbreezy <khristopherabolarin@gmail.com>
2020-09-15 16:38:47 +0100

    Validation of signup and login forms complete

--- components/signup/signup.js
+++ components/signup/signup.js
@@ -1,36 +1,60 @@
+import { useForm } from 'react-hook-form';
+
+import FormInput from '../formInput/formInput';
 
 const Signup = () => {
+
+    const { register, handleSubmit, errors } = useForm();
+
+    const signupHandler = (data) =>  {
+        console.log(data);
+    };
+
     return (
         <>
             <div className="col-md-5">
                 <h3>Create New Account</h3>
                 <p>Create Your very own Kilimanjaro Account</p>
-                <form className="signup-form">
-                    <label htmlFor="fname">First Name</label>
-                    <div className="textbox">
-                        <input type="text" placeholder="First Name*" />
-                        <div className="border"></div>
-                    </div>
-                    <label htmlFor="lname">Last Name</label>
-                    <div className="textbox">
-                        <input type="text" placeholder="Last Name*" />
-                        <div className="border"></div>
-                    </div>
-                    <label htmlFor="email">Mobile Number</label>
-                    <div className="textbox">
-                        <input type="number" placeholder="+234 80 1234 5678" />
-                        <div className="border"></div>
-                    </div>
-                    <label htmlFor="email">Email Address</label>
-                    <div className="textbox">
-                        <input type="email" placeholder="Example@email.com*" />
-                        <div className="border"></div>
-                    </div>
-                    <label htmlFor="password">Password</label>
-                    <div className="textbox">
-                        <input type="password" placeholder="Password*" />
-                        <div className="border"></div>
-                    </div>
+                <form onSubmit={handleSubmit(signupHandler)} className="signup-form">
+                    <FormInput
+                        type="text"
+                        name="firstName"
+                        placeholder="First Name*"
+                        label="First Name"
+                        register={register ({required : true})} 
+                        error={errors.firstName && 'First name is required'}
+                    />
+                    <FormInput
+                        type="text"
+                        name="lastName"
+                        placeholder="Last Name*"
+                        label="Last Name"
+                        register={register ({required : true})}
+                        error={errors.lastName && 'Last name is required'} 
+                    />
+                    <FormInput
+                        type="number"
+                        name="mobileNumber"
+                        placeholder="+234 80 1234 5678"
+                        label="Mobile Number"
+                        register={register ({required : true})}
+                    />
+                    <FormInput
+                        type="email"
+                        name="email"
+                        placeholder="Example@email.com*"
+                        label="Email Address"
+                        register={register ({ required : true, pattern: /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/ })}
+                        error={errors.email && 'Please input a valid email address'} 
+                    />
+                    <FormInput
+                        type="password"
+                        name="password"
+                        placeholder="Password*"
+                        label="Password"
+                        register={register ({required : true, minLength: 8})}
+                        error={errors.password && 'Password must be more than 8 characters'} 
+                    />
                     <button className="btn btn-login mt-3">Register</button>
                 </form>
                 <p className="mt-3">Or sign up with</p>
