11fff6ef82dc1e5f9a63cdc647a137b207231161
khrisbreezy <khristopherabolarin@gmail.com>
2020-09-30 17:23:31 +0100

    Added the product categories

--- store/actions/shop.js
+++ store/actions/shop.js
@@ -1,10 +1,15 @@
 
-
 export const SAVE_RESTAURANTS = 'SAVE_RESTAURANTS';
+export const SELECTED_RESTAURANT = 'SELECTED_RESTAURANTS';
 
 export const saveRestaurants = (restaurants) => ({
     type: SAVE_RESTAURANTS,
     restaurants
+});
+
+export const selectedRestaurant = (selectRestaurant) => ({
+    type: SELECTED_RESTAURANT,
+    selectRestaurant
 });
 
 
