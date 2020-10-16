48f256717f0ae3fba24b1846d434ab136d306bb4
khrisbreezy <khristopherabolarin@gmail.com>
2020-10-16 16:03:31 +0100

    Added tthe store location

--- pages/store-location/index.js
+++ pages/store-location/index.js
@@ -20,91 +20,103 @@ const StoreLocation = () => {
                                 <h4>You can find us here</h4>
                             </div>
                             <div className="col-md-4 mb-5">
-                                <h5>Abia</h5>
-                                <ul>
-                                    <li>&ndash;Food Court, Abia Mall</li>
-                                </ul>
+                                <div className="card">
+                                    <img className="img-fluid" src="/images/abia.png" alt="" />
+                                    <div className="d-flex mt-5 align-items-center justify-content-between">
+                                        <h5>Abia</h5>
+                                        <button className="btn">
+                                            View Outlets
+                                    </button>
+                                    </div>
+                                </div>
                             </div>
                             <div className="col-md-4 mb-5">
-                                <h5>Lagos</h5>
-                                <ul>
-                                    <li>&ndash;84 Ozumba Mbadiwe Street, Victoria island.</li>
-                                    <li>&ndash;Murtala Muhammed International Airport Departure Lounge.</li>
-                                    <li>&ndash;Food Court, Novare Mall, Sangoredo.</li>
-                                    <li>&ndash;26B Admiralty way, Lekki Phase 1.</li>
-                                    <li>&ndash;175 Ago-palace way, Okota.</li>
-                                </ul>
+                                <div className="card">
+                                    <img className="img-fluid" src="/images/lagos.png" alt=""/>
+                                    <div className="d-flex mt-5 align-items-center justify-content-between">
+                                    <h5>Lagos</h5>
+                                    <button className="btn">
+                                        View Outlets
+                                    </button>
+                                    </div>
+                                </div>
                             </div>
                             <div className="col-md-4 mb-5">
-                                <h5>Portharcourt</h5>
-                                <ul>
-                                    <li>&ndash;1 Agip Road, Agip Roundabout, Rumeme.</li>
-                                    <li>&ndash;GRA Junction, Aba Road.</li>
-                                    <li>&ndash;222 Onne Road, GRA Phase II.</li>
-                                    <li>&ndash;YKC Junction, Woji Road.</li>
-                                    <li>&ndash;Bewac Junction, T/A Road.</li>
-                                    <li>&ndash;Uniport Choba</li>
-                                    <li>&ndash;Onne, FOT Roundabout</li>
-                                    <li>&ndash;Rumuibekwe Junction.</li>
-                                    <li>&ndash;Rumuokwuta Roundabout</li>
-                                    
-                                </ul>
+                                <div className="card">
+                                    <img className="img-fluid" src="/images/Delta-Mall.png" alt="" />
+                                    <div className="d-flex mt-5 align-items-center justify-content-between">
+                                        <h5>Delta</h5>
+                                        <button className="btn">
+                                            View Outlets
+                                    </button>
+                                    </div>
+                                </div>
                             </div>
                             <div className="col-md-4 mb-5">
-                                <h5>Abuja</h5>
-                                <ul>
-                                    <li>&ndash;911 Mall, 70 Usuma Street, Maitama</li>
-                                    <li>&ndash;Plot 84, 3rd Avenue, Gwasrinpa</li>
-                                    <li>&ndash;Aminu Kano Crescent, Wuse 2</li>
-                                    <li>&ndash;Novare Mall, Wuse, Zone 5</li>
-                                    <li>&ndash;Gado Nasko way, Kubwa</li>                                    
-                                </ul>
+                                <div className="card">
+                                    <img className="img-fluid" src="/images/abuja" alt="" />
+                                    <div className="d-flex mt-5 align-items-center justify-content-between">
+                                        <h5>Delta</h5>
+                                        <button className="btn">
+                                            View Outlets
+                                    </button>
+                                    </div>
+                                </div>
                             </div>
                             <div className="col-md-4 mb-5">
-                                <h5>Ogun</h5>
-                                <ul>
-                                    <li>&ndash;Food Court, The Palms Mall, Sango-Ota</li>                                   
-                                </ul>
+                                <div className="card">
+                                    <img className="img-fluid" src="/images/lagos.png" alt="" />
+                                    <div className="d-flex mt-5 align-items-center justify-content-between">
+                                        <h5>Delta</h5>
+                                        <button className="btn">
+                                            View Outlets
+                                    </button>
+                                    </div>
+                                </div>
                             </div>
                             <div className="col-md-4 mb-5">
-                                <h5>Delta</h5>
-                                <ul>
-                                    <li>&ndash;Food Court, Delta Mall, Efurrun</li>
-                                    <li>&ndash;Food Court, Asaba Mall</li>                                   
-                                </ul>
+                                <div className="card">
+                                    <img className="img-fluid" src="/images/lagos.png" alt="" />
+                                    <div className="d-flex mt-5 align-items-center justify-content-between">
+                                        <h5>Delta</h5>
+                                        <button className="btn">
+                                            View Outlets
+                                    </button>
+                                    </div>
+                                </div>
                             </div>
                             <div className="col-md-4 mb-5">
-                                <h5>Onitsha</h5>
-                                <ul>
-                                    <li>&ndash;Food Court, Onitsha Mall, ABS junction, off Awka road, GRA.</li>                                 
-                                </ul>
+                                <div className="card">
+                                    <img className="img-fluid" src="/images/lagos.png" alt="" />
+                                    <div className="d-flex mt-5 align-items-center justify-content-between">
+                                        <h5>Delta</h5>
+                                        <button className="btn">
+                                            View Outlets
+                                    </button>
+                                    </div>
+                                </div>
                             </div>
                             <div className="col-md-4 mb-5">
-                                <h5>Uyo</h5>
-                                <ul>
-                                    <li>&ndash;165 Oron Road, Uyo.</li>
-                                    <li>&ndash;189 Ikot Ekpene Road, Uyo.</li>                                 
-                                </ul>
+                            <div className="card">
+                                    <img className="img-fluid" src="/images/lagos.png" alt="" />
+                                    <div className="d-flex mt-5 align-items-center justify-content-between">
+                                        <h5>Delta</h5>
+                                        <button className="btn">
+                                            View Outlets
+                                    </button>
+                                    </div>
+                                </div>
                             </div>
                             <div className="col-md-4 mb-5">
-                                <h5>Enugu</h5>
-                                <ul>
-                                    <li>&ndash;Polo park Mall, Abakaliki Road, Old GRA.</li>
-                                    <li>&ndash;Enugu Mall, Independence Layout. Enugu</li>                                 
-                                </ul>
-                            </div>
-                            <div className="col-md-4 mb-5">
-                                <h5>Owerri</h5>
-                                <ul>
-                                    <li>&ndash;Shirley Supremo Shopping Mall, <br/> 32 Ekwema Crescent, Ikenegbu Layout</li>
-                                    <li>&ndash;Food Court, Owerri Mall, Egbu road.</li>                                 
-                                </ul>
-                            </div>
-                            <div className="col-md-4 mb-5">
-                                <h5>Yenogoa</h5>
-                                <ul>
-                                    <li>&ndash;Kilimanjaro Building, Opp Ekeki Park, Mbiama Road</li>                                 
-                                </ul>
+                                <div className="card">
+                                    <img className="img-fluid" src="/images/lagos.png" alt="" />
+                                    <div className="d-flex mt-5 align-items-center justify-content-between">
+                                        <h5>Delta</h5>
+                                        <button className="btn">
+                                            View Outlets
+                                    </button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
