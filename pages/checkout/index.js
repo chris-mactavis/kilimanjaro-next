52155f9fdccc847f58e173c54743ec60e94e81a4
Olaide Abdulazeez <dj3plles@gmail.com>
2020-10-22 18:32:09 +0100

    Load payment gateway

--- pages/checkout/index.js
+++ pages/checkout/index.js
@@ -8,7 +8,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import Cookies from 'js-cookie';
 import { NotificationManager } from 'react-notifications';
 
-
 import OrderingSteps from '../../components/orders/orderingSteps/orderingSteps';
 import FormInput from '../../components/formInput/formInput';
 import axiosInstance from '../../config/axios';
@@ -121,7 +120,7 @@ const Checkout = () => {
                 }
             }
 
-            if ((paymentOption === 'delivery' && paymentMethod === 'pay online') || (paymentOption === 'pickup' && paymentMethod === 'pay online')) {
+            if ((paymentOption === 'delivery' && paymentMethod === 'payment online') || (paymentOption === 'pickup' && paymentMethod === 'payment online')) {
                 FlutterwaveCheckout({
                     public_key: "FLWPUBK_TEST-fe28dc780f5dd8699e9ac432c33c036e-X",
                     tx_ref: "hooli-tx-1920bbtyt",
@@ -129,8 +128,7 @@ const Checkout = () => {
                     currency: "NGN",
                     country: "NG",
                     payment_options: "card, mobilemoneyghana, ussd",
-                    redirect_url: // specified redirect URL
-                        Route.push('/order-complete'),
+                    // redirect_url: '/order-complete',
                     meta: {
                         consumer_id: 23,
                         consumer_mac: "92a3-912ba-1192a",
@@ -141,6 +139,7 @@ const Checkout = () => {
                         name: user.first_name + user.last_name,
                     },
                     callback: async (data) => {
+                        console.log(data);
                         try {
                             const data = await axiosInstance.post('orders', orderData);
                             const orderItem = data.data.data; 
@@ -170,6 +169,7 @@ const Checkout = () => {
                     },
                 });
             }
+            return;
 
             try {
                 const data = await axiosInstance.post('orders', orderData);
